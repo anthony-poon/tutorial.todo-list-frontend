bd69bf71b4edf7d20b04eec04fd47af2a134e65d
Anthony Poon <ypoon196@gmail.com>
2021-01-09 00:23:38 +0800

    Initial state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = {
+        todos: [],
+        input: ""
+    }
+
+    handleChange(evt) {
+        const {
+            name,
+            value
+        } = evt.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleAdd(evt) {
+        evt.preventDefault();
+        const {
+            todos,
+            input
+        } = this.state;
+        this.setState({
+            todos: [
+                ...todos,
+                input
+            ],
+            input: ""
+        })
+    }
+
+    render() {
+        const {
+            todos,
+            input
+        } = this.state;
+        return (
+            <div className={"container"}>
+                <div className={"row mt-5"}>
+                    <div className={"col-8"}>
+                        <h5 className={"mb-3"}>To Do List:</h5>
+                        <form onSubmit={evt => this.handleAdd(evt)}>
+                            <div className={"row mb-3"}>
+                                <div className={"col"}>
+                                    <input
+                                        type={"text"}
+                                        className={"form-control"}
+                                        value={input}
+                                        name={"input"}
+                                        onChange={evt => this.handleChange(evt)}
+                                        autoComplete={"off"}
+                                    />
+                                </div>
+                                <div className={"col-auto"}>
+                                    <button type={"button"} className={"btn btn-primary"} onClick={() => this.handleAdd()}>
+                                        Add
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                        <ul>
+                            {
+                                todos.map((todo, index) => (
+                                    <li key={index} className={"my-2"}>
+                                        { todo }
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
